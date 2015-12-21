495ca2f15b1ad7b0403e0d0bfcb0c935efc7b28d
Patrick Stein <pat@nklein.com>
2015-12-21 12:04:34 -0600

    Fixed name of stream used for testing

--- utf8/test/output.lisp
+++ utf8/test/output.lisp
@@ -20,19 +20,19 @@
       (:equalp #(97 98 99 100 101 102 103 104 105 106))
     (with-output-to-vector (out)
       (with-open-stream
-          (noop (tstreams-utf8:make-utf8-character-to-binary-output-tstream
+          (utf8 (tstreams-utf8:make-utf8-character-to-binary-output-tstream
                  out))
-        (write-string "abcd" noop)
-        (write-char #\e noop)
-        (write-char #\f noop)
-        (write-char #\g noop)
-        (format noop "hij"))))
+        (write-string "abcd" utf8)
+        (write-char #\e utf8)
+        (write-char #\f utf8)
+        (write-char #\g utf8)
+        (format utf8 "hij"))))
 
   (nst:def-test character-to-binary-utf8-on-unicode
       (:equalp #(207 128 32 226 160 143 226 160 138 44 32
                  206 184 32 226 160 174 226 160 158 226 160 129))
     (with-output-to-vector (out)
       (with-open-stream
-          (noop (tstreams-utf8:make-utf8-character-to-binary-output-tstream
+          (utf8 (tstreams-utf8:make-utf8-character-to-binary-output-tstream
                  out))
-        (write-string "π ⠏⠊, θ ⠮⠞⠁" noop)))))
+        (write-string "π ⠏⠊, θ ⠮⠞⠁" utf8)))))
